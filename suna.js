27dd813a88abc5778ff30179fdbb7783a18c8fb2
yeahyeahsatan <88475148+yeahyeahsatan@users.noreply.github.com>
2021-12-15 14:18:04 +0900

    Add files via upload

---
+++ suna.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    var swiper = new Swiper('#visual .swiper', {
+      
+      loop: true,
+      navigation: {
+        nextEl: '#visual .swiper-button-next',
+        prevEl: '#visual .swiper-button-prev',
+      },
+    
+     
+    });
+  
+    var swiper = new Swiper('#exclusive .swiper', {
+      
+      loop: true,
+      navigation: {
+        nextEl: '#exclusive .swiper-button-next',
+        prevEl: '#exclusive .swiper-button-prev',
+      },
+    
+     
+    });
+  
+  });
